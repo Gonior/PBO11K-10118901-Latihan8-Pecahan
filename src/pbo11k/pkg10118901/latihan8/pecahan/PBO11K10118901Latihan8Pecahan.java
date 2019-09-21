48874d08febf103cc0f56599ad33a0fbc935a91e
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-21 23:38:01 +0700

    Dibuat : Sabtu, 21 September 2019

---
+++ src/pbo11k/pkg10118901/latihan8/pecahan/PBO11K10118901Latihan8Pecahan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * NAMA         : Dedi Cahya
+ * KELAS        : IF-11-K
+ * NIM          : 10118901
+ * DESKRIPSI    : Mengenan lebih jauh tipe data float (pecahan)
+ */
+public class PBO11K10118901Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float :" +f);
+        System.out.println("double :" +d);
+        
+    }
+    
+}
